8bd3609e02f34aec2c73259718bf23deb0c25700
Wale Timothy <awtim01@gmail.com>
2019-10-11 13:45:47 +0100

    changed mongodb link to a live link rather than local db

--- config/db.js
+++ config/db.js
@@ -1,4 +1,5 @@
 module.exports = {
     // url:"mongodb://wale-tim:random123@cluster0-shard-00-00-b9qvi.mongodb.net:27017,cluster0-shard-00-01-b9qvi.mongodb.net:27017,cluster0-shard-00-02-b9qvi.mongodb.net:27017/admin?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority"
-    url:"mongodb://localhost:27017/note-api"
+    url:"mongodb+srv://wale-tim:random123@cluster0-b9qvi.mongodb.net/test?retryWrites=true&w=majority"
+    // url:"mongodb://localhost:27017/note-api"
 }
